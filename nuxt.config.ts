b1140f90f0454ebcc2c4b33c7f0ab8233f495293
alkaprog <superpupergametsar@gmail.com>
2024-08-29 15:00:59 +0300

    Added Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,27 @@
+import { fileURLToPath, URL } from "url";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  alias: {
+    app: fileURLToPath(new URL("./src/app", import.meta.url)),
+    entities: fileURLToPath(new URL("./src/entities", import.meta.url)),
+    pages: fileURLToPath(new URL("./src/pages", import.meta.url)),
+    widgets: fileURLToPath(new URL("./src/widgets", import.meta.url)),
+    features: fileURLToPath(new URL("./src/features", import.meta.url)),
+    shared: fileURLToPath(new URL("./src/shared", import.meta.url)),
+  },
+  dir: {
+    pages: "./src/pages",
+    layouts: "./src/app/layouts",
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: ["~/src/shared/styles/main.css"],
   build: {
     transpile: ["vuetify"],
   },
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,vue,ts}",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
