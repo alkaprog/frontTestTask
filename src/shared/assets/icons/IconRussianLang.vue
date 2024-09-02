676f3429a915ec8f480a3bce97878dd85d3519d3
alkaprog <superpupergametsar@gmail.com>
2024-09-02 08:43:18 +0300

    added icons

---
+++ src/shared/assets/icons/IconRussianLang.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="16" height="5.33333" fill="white" />
+    <rect y="5.33333" width="16" height="5.33333" fill="#0945E0" />
+    <rect y="10.6667" width="16" height="5.33333" fill="#DB0606" />
+  </svg>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/shared/assets/icons/index.ts
@@ -0,0 +1,7 @@
+export { default as IconCart } from "./IconCart.vue";
+export { default as IconCategories } from "./IconCategories.vue";
+export { default as IconLocation } from "./IconLocation.vue";
+export { default as IconLogo } from "./IconLogo.vue";
+export { default as IconRussianLang } from "./IconRussianLang.vue";
+export { default as IconApproved } from "./IconApproved.vue";
+export { default as IconStar } from "./IconStar.vue";
