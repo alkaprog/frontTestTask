c1738b63d6bf2bf1b3e21410068a26b8da0294c4
alkaprog <superpupergametsar@gmail.com>
2024-09-04 09:20:54 +0300

    Added base layout

---
+++ src/app/layouts/MainLayout.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { CompanyBaseInfo } from "widgets";
+import { type Company, getCompanyInfo } from "entities/company";
+import { VHeader, VFooter, useDeviceDetector } from "shared";
+import {
+  defaultFooterItems,
+  navMenuItems,
+  routesToShowBaseInfoOnMobile,
+} from "app";
+
+const { isDesktop } = useDeviceDetector();
+const route = useRoute();
+
+const company = ref<Company | null>(null);
+const activeNavItem = ref(
+  navMenuItems.findIndex((item) => item.url === route.path),
+);
+
+const footerItems = computed(() =>
+  isDesktop.value ? defaultFooterItems.slice(0, 2) : defaultFooterItems,
+);
+const isBaseInfoShownOnMobile = computed(() =>
+  routesToShowBaseInfoOnMobile.includes(route.path),
+);
+
+const fetchCompany = async () => {
+  const { data, status } = await getCompanyInfo();
+  if (status.value === "success" && data.value) {
+    company.value = data.value;
+  }
+};
+
+await fetchCompany();
+
+provide("company", company);
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <v-header />
+    <div class="flex flex-col items-center grow mt-[56px] xl:mt-[64px]">
+      <div class="w-full flex flex-col xl:flex-col-reverse">
+        <client-only>
+          <nav class="py-2 border-b border-solid border-border-light xl:py-8">
+            <v-slide-group v-model="activeNavItem">
+              <v-slide-group-item
+                v-for="(navMenuItem, index) in navMenuItems"
+                :key="`nav-item-${index}`"
+                v-slot="{ toggle }"
+              >
+                <nuxt-link
+                  class="flex items-center bg-bg-grey px-3 h-10 rounded-[12px] font-medium text-sm"
+                  :class="{
+                    'ml-4': index === 0,
+                    'mr-4': index === navMenuItems.length - 1,
+                  }"
+                  active-class="nav-item__active"
+                  :to="navMenuItem.url"
+                  @click="toggle"
+                >
+                  {{ navMenuItem.name }}
+                </nuxt-link>
+              </v-slide-group-item>
+            </v-slide-group>
+          </nav>
+        </client-only>
+        <company-base-info
+          :class="{
+            'hidden xl:flex': !isBaseInfoShownOnMobile,
+          }"
+          :company="company"
+        />
+      </div>
+      <slot class="w-full" />
+    </div>
+    <v-footer :footer-items="footerItems" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+:deep(.v-slide-group__content) {
+  @apply gap-[11px] justify-center;
+}
+
+.nav-item__active {
+  @apply border-[1px] border-solid !px-[11px];
+}
+</style>
---
+++ src/app/model/const.ts
@@ -0,0 +1,39 @@
+export const navMenuItems = [
+  {
+    name: "Товары и услуги",
+    url: "/goods",
+  },
+  {
+    name: "Агенты",
+    url: "/agents",
+  },
+  {
+    name: "О компании",
+    url: "/about",
+  },
+];
+
+export const defaultFooterItems = [
+  {
+    name: "Документы",
+    url: "/",
+  },
+  {
+    name: "О компании",
+    url: "/about",
+  },
+  {
+    name: "Продавцам",
+    url: "/",
+  },
+  {
+    name: "Покупателям",
+    url: "/",
+  },
+  {
+    name: "Разместить объявление",
+    url: "/",
+  },
+];
+
+export const routesToShowBaseInfoOnMobile = ["/about"];
