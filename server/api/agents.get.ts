89dbc9658537830f05ad099dd725024674332309
alkaprog <superpupergametsar@gmail.com>
2024-09-05 17:28:40 +0300

    Close agents list for not authorized users

--- server/api/agents.get.ts
+++ server/api/agents.get.ts
@@ -1,30 +1,41 @@
-export default defineEventHandler(() => {
-  return [
-    {
-      id: 1,
-      name: "Василий Копушин",
-      phone: "+79232233442",
-      description:
-        "Помог справиться с потерей и организовать похороны для 126 семей ну и еще чуть описания, чтобы дойти до конца строки и текс",
-      rating: 4.3,
-      reviewsCount: 17,
-      isApproved: true,
-      photo:
-        "https://gas-kvas.com/grafic/uploads/posts/2023-10/1696520592_gas-kvas-com-p-kartinki-kvadratnie-3.jpg",
-      price: 100,
-    },
-    {
-      id: 2,
-      name: "Василий Копушин",
-      phone: "+79232233442",
-      description:
-        "Помог справиться с потерей и организовать похороны для 126 семей ну и еще чуть описания, чтобы дойти до конца строки и текс",
-      rating: 4.3,
-      reviewsCount: 17,
-      isApproved: true,
-      photo:
-        "https://gas-kvas.com/grafic/uploads/posts/2023-10/1696520592_gas-kvas-com-p-kartinki-kvadratnie-3.jpg",
-      price: 100,
-    },
-  ];
+import { isAccessTokenValid } from "~/server/helpers";
+
+export default defineEventHandler((event) => {
+  const { authorization } = getHeaders(event);
+  const token = authorization?.slice(7);
+
+  if (authorization && isAccessTokenValid(token as string)) {
+    return [
+      {
+        id: 1,
+        name: "Василий Копушин",
+        phone: "+79232233442",
+        description:
+          "Помог справиться с потерей и организовать похороны для 126 семей ну и еще чуть описания, чтобы дойти до конца строки и текс",
+        rating: 4.3,
+        reviewsCount: 17,
+        isApproved: true,
+        photo:
+          "https://gas-kvas.com/grafic/uploads/posts/2023-10/1696520592_gas-kvas-com-p-kartinki-kvadratnie-3.jpg",
+        price: 100,
+      },
+      {
+        id: 2,
+        name: "Василий Копушин",
+        phone: "+79232233442",
+        description:
+          "Помог справиться с потерей и организовать похороны для 126 семей ну и еще чуть описания, чтобы дойти до конца строки и текс",
+        rating: 4.3,
+        reviewsCount: 17,
+        isApproved: true,
+        photo:
+          "https://gas-kvas.com/grafic/uploads/posts/2023-10/1696520592_gas-kvas-com-p-kartinki-kvadratnie-3.jpg",
+        price: 100,
+      },
+    ];
+  }
+  throw createError({
+    statusCode: 401,
+    statusMessage: "You should login",
+  });
 });
--- src/entities/agent/api/index.ts
+++ src/entities/agent/api/index.ts
@@ -2,5 +2,5 @@ import { useRequest } from "shared";
 import type { Agent } from "entities/agent";
 
 export const getAgentList = () => {
-  return useRequest<Agent[]>("/api/agents");
+  return useRequest<Agent[]>("/api/agents", {}, true);
 };
