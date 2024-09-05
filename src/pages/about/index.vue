e28cbf477da5f168dd916d4a159a1c44bd133cc3
alkaprog <superpupergametsar@gmail.com>
2024-09-05 13:46:38 +0300

    Created About Page

---
+++ src/pages/about/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { CompanyDescription, CompanyLocation, CompanySchedule } from "widgets";
+import type { Company } from "entities/company";
+
+definePageMeta({
+  layout: "main-layout",
+});
+
+const company = inject<Company>("company");
+</script>
+
+<template>
+  <div class="flex justify-center pb-12 xl:pt-8 xl:pb-[64px]">
+    <div
+      v-if="company"
+      class="flex flex-col gap-2 w-full xl:w-[560px] xl:gap-3"
+    >
+      <company-description :company="company" />
+      <company-schedule :company="company" />
+      <company-location :company="company" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
